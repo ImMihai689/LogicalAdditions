690c77ad328163ded73fa37c9fbcad13fccc7e1d
Hoflett <75139772+ImMihai689@users.noreply.github.com>
2023-07-03 01:35:17 +0300

    Added the mod files again

---
+++ src/server/RandomDebugCollection.cs
@@ -0,0 +1,16 @@
+using LogicAPI.Server.Components;
+using LogicAPI.Server;
+using LogicLog;
+using LogicWorld.Server.Circuitry;
+
+namespace LA
+{
+	public class RandomDebugCollection : ServerMod
+	{
+		
+
+		protected override void Initialize()
+		{
+		}
+	}
+}
